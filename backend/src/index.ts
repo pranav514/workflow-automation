58b194f2543bcb3f32460dcc74b8f046346ee875
pranav514 <pranavsalunkhe327@gmail.com>
2025-03-04 12:08:00 +0530

    feat : added trigger and action with cache

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express from "express"
 import cors from "cors"
 import userRoute from "./routes/userRoute";
+import actionRoute from "./routes/actionRoute";
+import triggerRoute from "./routes/triggerRoute";
+import { redisClient } from "./config";
 const app = express();
 app.use(express.json());
 app.use(cors())
 app.use('/api/v1/user' , userRoute)
-app.listen(3001 , () => {
+app.use('/api/v1/action' , actionRoute)
+app.use('/api/v1/trigger' ,triggerRoute)
+app.listen(3001 , async() => {
+    await redisClient.connect();
     console.log("server of primary backend started on port 3001")
 })
